984b98118c9f2ac123cec00f73c9f69122b1e6ca
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-03-11 12:11:20 -0300

    i add item.model.ts into item folder into folder model into folder infrastructure to project

---
+++ src/infrastructure/model/item/item.model.ts
@@ -0,0 +1,52 @@
+import { DataTypes, Model } from 'sequelize';
+import { sequelize } from '../../db/sequelize';
+import { ItemEntity } from "../../../domain/item/item.entity";
+
+export class SequelizeItem extends Model<ItemEntity, Omit<ItemEntity, 'id'>> {
+  declare cmp_uuid: string;
+  declare itm_uuid: string;
+  declare itm_name: string;
+  declare itm_description: string;
+  declare itm_createdat: Date;
+  declare itm_updatedat: Date;
+}
+
+SequelizeItem.init({
+  cmp_uuid: {
+    type: DataTypes.STRING, 
+    primaryKey: true/*,
+    autoIncrement: true*/
+  },
+  itm_uuid: {
+    type: DataTypes.STRING, 
+    primaryKey: true/*,
+    autoIncrement: true*/
+  },
+  itm_name: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  itm_description: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  itm_createdat: {
+    type: DataTypes.DATE,
+    allowNull: true
+  },
+  itm_updatedat: {
+    type: DataTypes.DATE,
+    allowNull: true
+  }
+}, {
+  sequelize,
+  timestamps: true,
+  createdAt: 'itm_createdat', 
+  updatedAt: 'itm_updatedat',
+  tableName: 'itm_items'
+});
+
+// Sincronizar (solo en desarrollo)
+if (process.env.NODE_ENV !== "itemion") {
+    SequelizeItem.sync();
+}
