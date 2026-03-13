a539044a5d199ca41542fa93a7b13d0a9b274472
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-03-13 16:27:31 -0300

    i add company.value.ts into company folder into folder domain to project

---
+++ src/domain/company/company.value.ts
@@ -0,0 +1,63 @@
+import { v4 as uuid } from "uuid";
+import moment from 'moment';
+import { CompanyEntity } from "./company.entity";
+
+export class CompanyValue implements CompanyEntity {
+    cmp_uuid: string;
+    cmp_name: string;
+    cmp_address: string;    
+    cmp_phone: string;
+    cmp_email: string;
+    cmp_slug: string;
+    cmp_logo: string;
+    cmp_banner: string;
+    cmp_description: string;
+    cmp_isfeatured: boolean;
+    cmp_status: string;  //-- active, inactive, pending
+    cmp_createdat: Date;
+    cmp_updatedat: Date;
+    
+    constructor({
+            cmp_uuid,
+            cmp_name,
+            cmp_address,    
+            cmp_phone,
+            cmp_email,
+            cmp_slug,
+            cmp_logo,
+            cmp_banner,
+            cmp_description,
+            cmp_isfeatured,
+            cmp_status,
+            cmp_createdat,
+            cmp_updatedat
+        }:{ 
+            cmp_uuid: string,
+            cmp_name: string,
+            cmp_address: string,    
+            cmp_phone: string,
+            cmp_email: string,
+            cmp_slug: string,
+            cmp_logo: string,
+            cmp_banner: string,
+            cmp_description: string,
+            cmp_isfeatured: boolean,
+            cmp_status: string,
+            cmp_createdat?: Date,
+            cmp_updatedat?: Date
+        }) {
+        this.cmp_uuid = uuid();
+        this.cmp_name = cmp_name;
+        this.cmp_address = cmp_address;    
+        this.cmp_phone = cmp_phone;
+        this.cmp_email = cmp_email;
+        this.cmp_slug = cmp_slug;
+        this.cmp_logo = cmp_logo;
+        this.cmp_banner = cmp_banner;
+        this.cmp_description = cmp_description;
+        this.cmp_isfeatured = cmp_isfeatured;
+        this.cmp_status = cmp_status;
+        this.cmp_createdat = cmp_createdat ?? moment().toDate();
+        this.cmp_updatedat = cmp_updatedat ?? moment().toDate();
+    }
+}
