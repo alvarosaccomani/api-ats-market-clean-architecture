f4bc92d1fe2aa6ce502cbb043a8819771e8f914c
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-03-13 16:28:28 -0300

    i add company.model.ts into company folder into folder model into folder infrastructure to project

---
+++ src/infrastructure/model/company/company.model.ts
@@ -0,0 +1,86 @@
+import { DataTypes, Model } from 'sequelize';
+import { sequelize } from '../../db/sequelize';
+import { CompanyEntity } from "../../../domain/company/company.entity";
+
+export class SequelizeCompany extends Model<CompanyEntity, Omit<CompanyEntity, 'id'>> {
+  declare cmp_uuid: string;
+  declare cmp_name: string;
+  declare cmp_address: string;
+  declare cmp_phone: string;
+  declare cmp_email: string;
+  declare cmp_slug: string;
+  declare cmp_logo: string;
+  declare cmp_banner: string;
+  declare cmp_description: string;
+  declare cmp_isfeatured: boolean;
+  declare cmp_status: string;  //-- active, inactive, pending
+  declare cmp_createdat: Date;
+  declare cmp_updatedat: Date;
+}
+
+SequelizeCompany.init({
+  cmp_uuid: {
+    type: DataTypes.STRING, 
+    primaryKey: true/*,
+    autoIncrement: true*/
+  },
+  cmp_name: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  cmp_address: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  cmp_phone: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  cmp_email: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  cmp_slug: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  cmp_logo: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  cmp_banner: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  cmp_description: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  cmp_isfeatured: {
+    type: DataTypes.BOOLEAN,
+    allowNull: true
+  },
+  cmp_status: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  cmp_createdat: {
+    type: DataTypes.DATE,
+    allowNull: true
+  },
+  cmp_updatedat: {
+    type: DataTypes.DATE,
+    allowNull: true
+  }
+}, {
+  sequelize,
+  timestamps: true,
+  createdAt: 'cmp_createdat', 
+  updatedAt: 'cmp_updatedat',
+  tableName: 'cmp_companies'
+});
+
+// Sincronizar (solo en desarrollo)
+if (process.env.NODE_ENV !== "itemion") {
+    SequelizeCompany.sync();
+}
