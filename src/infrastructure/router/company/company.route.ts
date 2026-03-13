cdc84c756ecf2ee70f29ea8d25a90de6e37974b9
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-03-13 16:31:06 -0300

    i add company.route.ts into company folder into folder router into folder infrastructure to project

---
+++ src/infrastructure/router/company/company.route.ts
@@ -0,0 +1,35 @@
+import { Express } from "express";
+import { SequelizeRepository as SequelizeCompanyRepository } from "../../repository/company/sequelize-company.repository";
+import { CompanyUseCase } from "../../../application/company/company-use-case";
+import { CompanyController } from "../../controller/company/company.controller";
+import SocketAdapter from "../../services/socketAdapter";
+
+function configureCompanyRoutes(app: Express, socketAdapter: SocketAdapter) {
+    /*
+    *   Iniciar repository
+    */
+    
+    const sequelizeCompanyRepository = new SequelizeCompanyRepository();
+    
+    /*
+    *   Iniciar casos de uso
+    */
+    
+    const companyUseCase = new CompanyUseCase(sequelizeCompanyRepository);
+    
+    /*
+    *   Iniciar controller
+    */
+    
+    const companyCtrl = new CompanyController(companyUseCase, socketAdapter);
+    
+    app.get(`/${process.env.BASE_URL_API}/companies/:filter?/:page?/:perPage?`, companyCtrl.getAllCtrl);
+    app.get(`/${process.env.BASE_URL_API}/company/:cmp_uuid`, companyCtrl.getCtrl);
+    app.post(`/${process.env.BASE_URL_API}/company`, companyCtrl.insertCtrl);
+    app.put(`/${process.env.BASE_URL_API}/company/:cmp_uuid`, companyCtrl.updateCtrl);
+    app.delete(`/${process.env.BASE_URL_API}/company/:cmp_uuid`, companyCtrl.deleteCtrl);
+    app.get(`/${process.env.BASE_URL_API}/company-by-slug/:cmp_slug`, companyCtrl.getBySlugCtrl);
+    app.get(`/${process.env.BASE_URL_API}/featured-companies`, companyCtrl.getFeaturedCompaniesCtrl);
+}
+
+export default configureCompanyRoutes;
