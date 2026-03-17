f479a8c0e84effef7829809b1e7d6912684702ac
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-03-17 16:58:58 -0300

    i add global-item.value.ts into global-item folder into folder domain to project

---
+++ src/domain/global-item/global-item.value.ts
@@ -0,0 +1,31 @@
+import { v4 as uuid } from "uuid";
+import moment from 'moment';
+import { GlobalItemEntity } from "./global-item.entity";
+
+export class GlobalItemValue implements GlobalItemEntity {    
+    gitm_uuid: string;
+    gitm_name: string;
+    gitm_description: string;
+    gitm_createdat: Date;
+    gitm_updatedat: Date;
+    
+    constructor({
+            gitm_uuid,
+            gitm_name,
+            gitm_description,
+            gitm_createdat,
+            gitm_updatedat
+        }:{ 
+            gitm_uuid: string,
+            gitm_name: string,
+            gitm_description: string,
+            gitm_createdat?: Date,
+            gitm_updatedat?: Date
+        }) {
+        this.gitm_uuid = uuid();
+        this.gitm_name = gitm_name;
+        this.gitm_description = gitm_description;
+        this.gitm_createdat = gitm_createdat ?? moment().toDate();
+        this.gitm_updatedat = gitm_updatedat ?? moment().toDate();
+    }
+}
