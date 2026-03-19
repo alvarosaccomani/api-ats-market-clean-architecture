962b72b17b9aa55e37a5f06aca89683cd9465865
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-03-19 18:27:39 -0300

    i add user-rol-company.repository.ts into user-rol-company folder into folder domain to project

---
+++ src/domain/user-rol-company/user-rol-company.repository.ts
@@ -0,0 +1,12 @@
+import { UserRolCompanyEntity } from "./user-rol-company.entity";
+
+export interface UserRolCompanyRepository {
+    getUserRolesCompany(cmp_uuid: string): Promise<UserRolCompanyEntity[] | null>;
+    findUserRolCompanyById(cmp_uuid: string, usr_uuid: string, rol_uuid: string): Promise<UserRolCompanyEntity | null>;
+    createUserRolCompany(userRolCompany: UserRolCompanyEntity): Promise<UserRolCompanyEntity | null>;
+    updateUserRolCompany(cmp_uuid: string, usr_uuid: string, rol_uuid: string, userRolCompany: UserRolCompanyEntity): Promise<UserRolCompanyEntity | null>;
+    deleteUserRolCompany(cmp_uuid: string, usr_uuid: string, rol_uuid: string): Promise<UserRolCompanyEntity | null>;
+    existUserRolCompany(cmp_uuid: string, usr_uuid: string, rol_uuid: string): Promise<UserRolCompanyEntity | null>;
+    getUserRolesCompanyByUser(usr_uuid: string): Promise<UserRolCompanyEntity[] | null>;
+    getUserRolesCompanyByCompanyUser(cmp_uuid: string, usr_uuid: string): Promise<UserRolCompanyEntity[] | null>;
+}
