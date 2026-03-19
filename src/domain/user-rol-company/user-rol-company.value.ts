d7c35887ad54151450b134705a54029a34645987
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-03-19 18:26:58 -0300

    i add user-rol-company.value.ts into user-rol-company folder into folder domain to project

---
+++ src/domain/user-rol-company/user-rol-company.value.ts
@@ -0,0 +1,31 @@
+import { v4 as uuid } from "uuid";
+import moment from 'moment';
+import { UserRolCompanyEntity } from "./user-rol-company.entity";
+
+export class UserRolCompanyValue implements UserRolCompanyEntity {
+    cmp_uuid: string;
+    usr_uuid: string;
+    rol_uuid: string;
+    usrrolcmp_createdat: Date;
+    usrrolcmp_updatedat: Date;
+    
+    constructor({
+            cmp_uuid,
+            usr_uuid,
+            rol_uuid,
+            usrrolcmp_createdat,
+            usrrolcmp_updatedat
+        }:{ 
+            cmp_uuid: string,
+            usr_uuid: string,
+            rol_uuid: string,
+            usrrolcmp_createdat?: Date,
+            usrrolcmp_updatedat?: Date
+        }) {
+        this.cmp_uuid = cmp_uuid;
+        this.usr_uuid = usr_uuid;
+        this.rol_uuid = rol_uuid;
+        this.usrrolcmp_createdat = usrrolcmp_createdat ?? moment().toDate();
+        this.usrrolcmp_updatedat = usrrolcmp_updatedat ?? moment().toDate();
+    }
+}
