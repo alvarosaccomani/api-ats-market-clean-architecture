6ceaa48843df527681401f2f4d7ea6bb74c0aed5
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-03-21 20:29:47 -0300

    i add customer.repository.ts into customer folder into folder domain to project

---
+++ src/domain/customer/customer.repository.ts
@@ -0,0 +1,10 @@
+import { CustomerEntity, CustomerUpdateData } from "./customer.entity";
+
+export interface CustomerRepository {
+    getCustomers(cmp_uuid: string): Promise<CustomerEntity[] | null>;
+    findCustomerById(cmp_uuid: string, cus_uuid: string): Promise<CustomerEntity | null>;
+    createCustomer(customer: CustomerEntity): Promise<CustomerEntity | null>;
+    updateCustomer(cmp_uuid: string, cus_uuid: string, customer: CustomerUpdateData): Promise<CustomerEntity | null>;
+    deleteCustomer(cmp_uuid: string, cus_uuid: string): Promise<CustomerEntity | null>;
+    findCustomerByName(cmp_uuid: string, cus_name: string, excludeUuid?: string | null): Promise<CustomerEntity | null>;
+}
