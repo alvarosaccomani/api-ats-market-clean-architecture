75bc125cc9c46a17518fda53e5d5d407aeeac597
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-03-23 11:20:42 -0300

    i add category.route.ts into category folder into folder router into folder infrastructure to project

---
+++ src/infrastructure/router/category/category.route.ts
@@ -0,0 +1,33 @@
+import { Express } from "express";
+import { SequelizeRepository as SequelizeCategoryRepository } from "../../repository/category/sequelize-category.repository";
+import { CategoryUseCase } from "../../../application/category/category-use-case";
+import { CategoryController } from "../../controller/category/category.controller";
+import SocketAdapter from "../../services/socketAdapter";
+
+function configureCategoryRoutes(app: Express, socketAdapter: SocketAdapter) {
+    /*
+    *   Iniciar repository
+    */
+    
+    const sequelizeCategoryRepository = new SequelizeCategoryRepository();
+    
+    /*
+    *   Iniciar casos de uso
+    */
+    
+    const categoryUseCase = new CategoryUseCase(sequelizeCategoryRepository);
+    
+    /*
+    *   Iniciar controller
+    */
+    
+    const categoryCtrl = new CategoryController(categoryUseCase, socketAdapter);
+    
+    app.get(`/${process.env.BASE_URL_API}/categories/:cmp_uuid/:itm_uuid/:filter?/:page?/:perPage?`, categoryCtrl.getAllCtrl);
+    app.get(`/${process.env.BASE_URL_API}/category/:cmp_uuid/:cat_uuid/:itm_uuid`, categoryCtrl.getCtrl);
+    app.post(`/${process.env.BASE_URL_API}/category`, categoryCtrl.insertCtrl);
+    app.put(`/${process.env.BASE_URL_API}/category/:cmp_uuid/:cat_uuid/:itm_uuid`, categoryCtrl.updateCtrl);
+    app.delete(`/${process.env.BASE_URL_API}/category/:cmp_uuid/:cat_uuid/:itm_uuid`, categoryCtrl.deleteCtrl);
+}
+
+export default configureCategoryRoutes;
