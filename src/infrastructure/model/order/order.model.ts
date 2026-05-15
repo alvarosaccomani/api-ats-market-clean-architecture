24d74b4569e710640d7b000aab1b6ba4594c3dd4
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-05-15 20:54:41 -0300

    i add order.model.ts into order folder into folder model into folder infrastructure to project

---
+++ src/infrastructure/model/order/order.model.ts
@@ -0,0 +1,102 @@
+import { DataTypes, Model } from 'sequelize';
+import { sequelize } from '../../db/sequelize';
+import { OrderEntity } from "../../../domain/order/order.entity";
+
+export class SequelizeOrder extends Model<OrderEntity, Omit<OrderEntity, 'id'>> {
+  declare cmp_uuid: string;
+  declare ord_uuid: string;
+  declare usr_uuid: string;
+  declare cus_uuid: string;
+  declare adr_uuid: string;
+  declare ord_ordernumber: number;
+  declare ord_status: string;
+  declare ord_date: Date;
+  declare ord_subtotal: number;
+  declare ord_shippingcost: number;
+  declare ord_tax: number;
+  declare ord_total: number;
+  declare ord_customernotes: string;
+  declare ord_trackingnumber: string;
+  declare ord_createdat: Date;
+  declare ord_updatedat: Date;
+}
+
+SequelizeOrder.init({
+  cmp_uuid: {
+    type: DataTypes.STRING, 
+    primaryKey: true/*,
+    autoIncrement: true*/
+  },
+  ord_uuid: {
+    type: DataTypes.STRING, 
+    primaryKey: true/*,
+    autoIncrement: true*/
+  },
+  usr_uuid: {
+    type: DataTypes.STRING, 
+    allowNull: true
+  },
+  cus_uuid: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  adr_uuid: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  ord_ordernumber: {
+    type: DataTypes.INTEGER,
+    allowNull: true
+  },
+  ord_status: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  ord_date: {
+    type: DataTypes.DATE,
+    allowNull: true
+  },
+  ord_subtotal: {
+    type: DataTypes.FLOAT,
+    allowNull: true
+  },
+  ord_shippingcost: {
+    type: DataTypes.FLOAT,
+    allowNull: true
+  },
+  ord_tax: {
+    type: DataTypes.FLOAT,
+    allowNull: true
+  },
+  ord_total: {
+    type: DataTypes.FLOAT,
+    allowNull: true
+  },
+  ord_customernotes: {
+    type: DataTypes.TEXT,
+    allowNull: true
+  },
+  ord_trackingnumber: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  ord_createdat: {
+    type: DataTypes.DATE,
+    allowNull: true
+  },
+  ord_updatedat: {
+    type: DataTypes.DATE,
+    allowNull: true
+  }
+}, {
+  sequelize,
+  timestamps: true,
+  createdAt: 'ord_createdat', 
+  updatedAt: 'ord_updatedat',
+  tableName: 'ord_orders'
+});
+
+// Sincronizar (solo en desarrollo)
+if (process.env.NODE_ENV !== "production") {
+    SequelizeOrder.sync();
+}
