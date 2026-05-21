8ec8ab14bc994e9e41e148ed7f25e3405ca695c0
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-05-21 13:17:31 -0300

    i add order-detail.controller.ts into order-detail folder into folder controller into folder infrastructure to project

---
+++ src/infrastructure/controller/order-detail/order-detail.controller.ts
@@ -0,0 +1,208 @@
+import { Request, Response } from "express";
+import { OrderDetailUseCase } from "../../../application/order-detail/order-detail-use-case";
+import SocketAdapter from "../../services/socketAdapter";
+import { paginator } from "../../services/paginator.service";
+
+export class OrderDetailController {
+    constructor(private orderDetailUseCase: OrderDetailUseCase, private socketAdapter: SocketAdapter) {
+        this.getAllCtrl = this.getAllCtrl.bind(this);
+        this.getCtrl = this.getCtrl.bind(this);
+        this.insertCtrl = this.insertCtrl.bind(this);
+        this.updateCtrl = this.updateCtrl.bind(this);
+        this.deleteCtrl = this.deleteCtrl.bind(this);
+    }
+
+    public async getAllCtrl(req: Request, res: Response) {
+        try {
+            const cmp_uuid = req.params.cmp_uuid;
+            const ord_uuid = req.params.ord_uuid;
+            const page = (req.params.page ? parseInt(req.params.page) : null);
+            const perPage = (req.params.perPage ? parseInt(req.params.perPage) : null);
+
+            if (!cmp_uuid || cmp_uuid.toLowerCase() === 'null' || cmp_uuid.toLowerCase() === 'undefined') {
+                return res.status(400).json({
+                    success: false,
+                    message: 'No se pudo recuperar los detalles.',
+                    error: 'Debe proporcionar un Id de company.'
+                });
+            }
+            if (!ord_uuid || ord_uuid.toLowerCase() === 'null' || ord_uuid.toLowerCase() === 'undefined') {
+                return res.status(400).json({
+                    success: false,
+                    message: 'No se pudo recuperar los detalles.',
+                    error: 'Debe proporcionar un Id de orden.'
+                });
+            }
+
+            if (page && perPage) {
+                const details = await this.orderDetailUseCase.getDetailOrders(cmp_uuid, ord_uuid);
+                return res.status(200).send({
+                    success: true,
+                    message: 'Detalles de orden retornados.',
+                    ...paginator(details, page, perPage)
+                });
+            } else {
+                const details = await this.orderDetailUseCase.getDetailOrders(cmp_uuid, ord_uuid);
+                return res.status(200).send({
+                    success: true,
+                    message: 'Detalles de orden retornados.',
+                    data: details
+                });
+            }
+        } catch (error: any) {
+            console.error('Error en getAllCtrl (controller):', error.message);
+            return res.status(400).json({
+                success: false,
+                message: 'No se pudo recuperar los detalles de orden.',
+                error: error.message,
+            });
+        }
+    }
+
+    public async getCtrl(req: Request, res: Response) {
+        try {
+            const cmp_uuid = req.params.cmp_uuid;
+            const ord_uuid = req.params.ord_uuid;
+            const ordd_uuid = req.params.ordd_uuid;
+
+            if (!cmp_uuid || cmp_uuid.toLowerCase() === 'null' || cmp_uuid.toLowerCase() === 'undefined') {
+                return res.status(400).json({
+                    success: false,
+                    message: 'No se pudo recuperar la direccion.',
+                    error: 'Debe proporcionar un Id de company.'
+                });
+            }
+            if (!ord_uuid || ord_uuid.toLowerCase() === 'null' || ord_uuid.toLowerCase() === 'undefined') {
+                return res.status(400).json({
+                    success: false,
+                    message: 'No se pudo recuperar la orden.',
+                    error: 'Debe proporcionar un Id de orden.'
+                });
+            }
+            if (!ordd_uuid || ordd_uuid.toLowerCase() === 'null' || ordd_uuid.toLowerCase() === 'undefined') {
+                return res.status(400).json({
+                    success: false,
+                    message: 'No se pudo recuperar la orden.',
+                    error: 'Debe proporcionar un Id de order-detail.'
+                });
+            }
+
+            const detail = await this.orderDetailUseCase.findDetailOrderById(cmp_uuid, ord_uuid, ordd_uuid);
+            return res.status(200).send({
+                success: true,
+                message: 'Detalle de orden retornado.',
+                data: detail
+            });
+        } catch (error: any) {
+            console.error('Error en getCtrl (controller):', error.message);
+            return res.status(400).json({
+                success: false,
+                message: 'No se pudo recuperar el detalle de orden.',
+                error: error.message,
+            });
+        }
+    }
+
+    public async insertCtrl({ body }: Request, res: Response) {
+        try {
+            const cmp_uuid = body.cmp_uuid;
+            const ord_uuid = body.ord_uuid;
+
+            if (!cmp_uuid || cmp_uuid.toLowerCase() === 'null' || cmp_uuid.toLowerCase() === 'undefined') {
+                return res.status(400).json({
+                    success: false,
+                    message: 'No se pudo recuperar la orden.',
+                    error: 'Debe proporcionar un Id de company.'
+                });
+            }
+            if (!ord_uuid || ord_uuid.toLowerCase() === 'null' || ord_uuid.toLowerCase() === 'undefined') {
+                return res.status(400).json({
+                    success: false,
+                    message: 'No se pudo recuperar la orden.',
+                    error: 'Debe proporcionar un Id de orden.'
+                });
+            }
+
+            const detail = await this.orderDetailUseCase.createDetailOrder(body);
+            return res.status(200).json({
+                success: true,
+                message: 'Detalle de orden insertado.',
+                data: detail
+            });
+        } catch (error: any) {
+            console.error('Error en insertCtrl (controller):', error.message);
+            return res.status(400).json({
+                success: false,
+                message: 'No se pudo insertar el detalle de orden.',
+                error: error.message,
+            });
+        }
+    }
+
+    public async updateCtrl(req: Request, res: Response) {
+        try {
+            const cmp_uuid = req.params.cmp_uuid;
+            const ord_uuid = req.params.ord_uuid;
+            const ordd_uuid = req.params.ordd_uuid;
+            const update = req.body;
+
+            const detail = await this.orderDetailUseCase.updateDetailOrder(cmp_uuid, ord_uuid, ordd_uuid, update);
+            return res.status(200).json({
+                success: true,
+                message: 'Detalle de orden actualizado.',
+                data: detail
+            });
+        } catch (error: any) {
+            console.error('Error en updateCtrl (controller):', error.message);
+            return res.status(400).json({
+                success: false,
+                message: 'No se pudo actualizar el detalle de orden.',
+                error: error.message,
+            });
+        }
+    }
+
+    public async deleteCtrl(req: Request, res: Response) {
+        try {
+            const cmp_uuid = req.params.cmp_uuid;
+            const ord_uuid = req.params.ord_uuid;
+            const ordd_uuid = req.params.ordd_uuid;
+
+            if (!cmp_uuid) {
+                return res.status(400).json({
+                    success: false,
+                    message: 'No se pudo eliminar el detalle.',
+                    error: 'Debe proporcionar un Id de company.'
+                });
+            }
+            if (!ord_uuid) {
+                return res.status(400).json({
+                    success: false,
+                    message: 'No se pudo eliminar el detalle.',
+                    error: 'Debe proporcionar un Id de orden.'
+                });
+            }
+            if (!ordd_uuid) {
+                return res.status(400).json({
+                    success: false,
+                    message: 'No se pudo eliminar el detalle.',
+                    error: 'Debe proporcionar un Id de order-detail.'
+                });
+            }
+
+            const detail = await this.orderDetailUseCase.deleteDetailOrder(cmp_uuid, ord_uuid, ordd_uuid);
+            return res.status(200).json({
+                success: true,
+                message: 'Detalle de orden eliminado.',
+                data: detail
+            });
+        } catch (error: any) {
+            console.error('Error en deleteCtrl (controller):', error.message);
+            return res.status(400).json({
+                success: false,
+                message: 'No se pudo eliminar el detalle de orden.',
+                error: error.message,
+            });
+        }
+    }
+}
