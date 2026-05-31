6e5334a70052551a294120bb40f4b385d34e853e
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-05-31 19:16:55 -0300

    i add order-history.repository.ts into order-history folder into folder domain to project

---
+++ src/domain/order-history/order-history.repository.ts
@@ -0,0 +1,9 @@
+import { OrderHistoryEntity, OrderHistoryUpdateData } from "./order-history.entity";
+
+export interface OrderHistoryRepository {
+    getOrdersHistory(cmp_uuid: string, ord_uuid: string): Promise<OrderHistoryEntity[] | null>;
+    findOrderHistoryById(cmp_uuid: string, ord_uuid: string, ordh_uuid: string): Promise<OrderHistoryEntity | null>;
+    createOrderHistory(orderHistory: OrderHistoryEntity): Promise<OrderHistoryEntity | null>;
+    updateOrderHistory(cmp_uuid: string, ord_uuid: string, ordh_uuid: string, orderHistory: OrderHistoryUpdateData): Promise<OrderHistoryEntity | null>;
+    deleteOrderHistory(cmp_uuid: string, ord_uuid: string, ordh_uuid: string): Promise<OrderHistoryEntity | null>;
+}
