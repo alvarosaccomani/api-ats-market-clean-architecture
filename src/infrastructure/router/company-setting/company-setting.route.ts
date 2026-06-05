39067561b1b573e8c29976509c9b8a3438187feb
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-06-04 22:16:15 -0300

    i add authMiddleware to company-setting.route.ts

--- src/infrastructure/router/company-setting/company-setting.route.ts
+++ src/infrastructure/router/company-setting/company-setting.route.ts
@@ -3,6 +3,7 @@ import { SequelizeRepository as SequelizeCompanySettingRepository } from "../../
 import { CompanySettingUseCase } from "../../../application/company-setting/company-setting-use-case";
 import { CompanySettingController } from "../../controller/company-setting/company-setting.controller";
 import SocketAdapter from "../../services/socketAdapter";
+import { authMiddleware } from "../../middleware/auth.middleware";
 
 function configureCompanySettingRoutes(app: Express, socketAdapter: SocketAdapter) {
     /*
@@ -25,9 +26,9 @@ function configureCompanySettingRoutes(app: Express, socketAdapter: SocketAdapte
     
     app.get(`/${process.env.BASE_URL_API}/companies-settings/:cmp_uuid/:filter?/:page?/:perPage?`, companySettingCtrl.getAllCtrl);
     app.get(`/${process.env.BASE_URL_API}/company-setting/:cmp_uuid/:cmps_uuid`, companySettingCtrl.getCtrl);
-    app.post(`/${process.env.BASE_URL_API}/company-setting`, companySettingCtrl.insertCtrl);
-    app.put(`/${process.env.BASE_URL_API}/company-setting/:cmp_uuid/:cmps_uuid`, companySettingCtrl.updateCtrl);
-    app.delete(`/${process.env.BASE_URL_API}/company-setting/:cmp_uuid/:cmps_uuid`, companySettingCtrl.deleteCtrl);
+    app.post(`/${process.env.BASE_URL_API}/company-setting`, authMiddleware, companySettingCtrl.insertCtrl);
+    app.put(`/${process.env.BASE_URL_API}/company-setting/:cmp_uuid/:cmps_uuid`, authMiddleware, companySettingCtrl.updateCtrl);
+    app.delete(`/${process.env.BASE_URL_API}/company-setting/:cmp_uuid/:cmps_uuid`, authMiddleware, companySettingCtrl.deleteCtrl);
 }
 
 export default configureCompanySettingRoutes;
