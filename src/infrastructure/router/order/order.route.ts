c957abf50ef712733df10e001bfec9eafcec90a4
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-06-04 22:16:57 -0300

    i add authMiddleware to order.route.ts

--- src/infrastructure/router/order/order.route.ts
+++ src/infrastructure/router/order/order.route.ts
@@ -5,6 +5,7 @@ import { SequelizeRepository as SequelizeOrderHistoryRepository } from "../../re
 import { OrderUseCase } from "../../../application/order/order-use-case";
 import { OrderController } from "../../controller/order/order.controller";
 import SocketAdapter from "../../services/socketAdapter";
+import { authMiddleware } from "../../middleware/auth.middleware";
 
 function configureOrderRoutes(app: Express, socketAdapter: SocketAdapter) {
     /*
@@ -27,12 +28,12 @@ function configureOrderRoutes(app: Express, socketAdapter: SocketAdapter) {
     
     const orderCtrl = new OrderController(orderUseCase, socketAdapter);
     
-    app.get(`/${process.env.BASE_URL_API}/orders/:cmp_uuid/:filter?/:page?/:perPage?`, orderCtrl.getAllCtrl);
-    app.get(`/${process.env.BASE_URL_API}/order/:cmp_uuid/:ord_uuid`, orderCtrl.getCtrl);
+    app.get(`/${process.env.BASE_URL_API}/orders/:cmp_uuid/:filter?/:page?/:perPage?`, authMiddleware, orderCtrl.getAllCtrl);
+    app.get(`/${process.env.BASE_URL_API}/order/:cmp_uuid/:ord_uuid`, authMiddleware, orderCtrl.getCtrl);
     app.post(`/${process.env.BASE_URL_API}/order`, orderCtrl.insertCtrl);
-    app.put(`/${process.env.BASE_URL_API}/order/:cmp_uuid/:ord_uuid`, orderCtrl.updateCtrl);
-    app.put(`/${process.env.BASE_URL_API}/change-order-status/:cmp_uuid/:ord_uuid`, orderCtrl.changeOrderStatusCtrl);
-    app.delete(`/${process.env.BASE_URL_API}/order/:cmp_uuid/:ord_uuid`, orderCtrl.deleteCtrl);
+    app.put(`/${process.env.BASE_URL_API}/order/:cmp_uuid/:ord_uuid`, authMiddleware, orderCtrl.updateCtrl);
+    app.put(`/${process.env.BASE_URL_API}/change-order-status/:cmp_uuid/:ord_uuid`, authMiddleware, orderCtrl.changeOrderStatusCtrl);
+    app.delete(`/${process.env.BASE_URL_API}/order/:cmp_uuid/:ord_uuid`, authMiddleware, orderCtrl.deleteCtrl);
     app.get(`/${process.env.BASE_URL_API}/orders-by-customer/:cus_uuid/:filter?/:page?/:perPage?`, orderCtrl.getOrdersByCustomer);
 }
 
