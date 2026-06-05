615bb5c144a252e66cf6219a458cdeda39ab368a
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-06-04 22:16:49 -0300

    i add authMiddleware to menu.route.ts

--- src/infrastructure/router/menu/menu.route.ts
+++ src/infrastructure/router/menu/menu.route.ts
@@ -2,6 +2,7 @@ import { Express } from "express";
 import { SequelizeRepository as SequelizeMenuRepository } from "../../repository/menu/sequelize-menu.repository";
 import { MenuUseCase } from "../../../application/menu/menu-use-case";
 import { MenuController } from "../../controller/menu/menu.controller";
+import { authMiddleware } from "../../middleware/auth.middleware";
 
 function configureMenuRoutes(app: Express) {
     /*
@@ -24,9 +25,9 @@ function configureMenuRoutes(app: Express) {
     app.get(`/${process.env.BASE_URL_API}/menus`, menuCtrl.getAllCtrl);
     app.get(`/${process.env.BASE_URL_API}/menu-tree`, menuCtrl.getMenuItemsCtrl);
     app.get(`/${process.env.BASE_URL_API}/menu/:mnu_uuid`, menuCtrl.getDetailCtrl);
-    app.post(`/${process.env.BASE_URL_API}/menu`, menuCtrl.insertCtrl);
-    app.put(`/${process.env.BASE_URL_API}/menu/:mnu_uuid`, menuCtrl.updateCtrl);
-    app.delete(`/${process.env.BASE_URL_API}/menu/:mnu_uuid`, menuCtrl.deleteCtrl);
+    app.post(`/${process.env.BASE_URL_API}/menu`, authMiddleware, menuCtrl.insertCtrl);
+    app.put(`/${process.env.BASE_URL_API}/menu/:mnu_uuid`, authMiddleware, menuCtrl.updateCtrl);
+    app.delete(`/${process.env.BASE_URL_API}/menu/:mnu_uuid`, authMiddleware, menuCtrl.deleteCtrl);
 }
 
 export default configureMenuRoutes;
