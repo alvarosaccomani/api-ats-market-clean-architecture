b23b75e29b577c87d882dfc39189f7b4c177c66b
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-06-04 22:17:17 -0300

    i add authMiddleware to warehouse.route.ts

--- src/infrastructure/router/warehouse/warehouse.route.ts
+++ src/infrastructure/router/warehouse/warehouse.route.ts
@@ -3,6 +3,7 @@ import { SequelizeRepository as SequelizeWarehouseRepository } from "../../repos
 import { WarehouseUseCase } from "../../../application/warehouse/warehouse-use-case";
 import { WarehouseController } from "../../controller/warehouse/warehouse.controller";
 import SocketAdapter from "../../services/socketAdapter";
+import { authMiddleware } from "../../middleware/auth.middleware";
 
 function configureWarehouseRoutes(app: Express, socketAdapter: SocketAdapter) {
     /*
@@ -20,11 +21,11 @@ function configureWarehouseRoutes(app: Express, socketAdapter: SocketAdapter) {
     */
     const warehouseCtrl = new WarehouseController(warehouseUseCase, socketAdapter);
     
-    app.get(`/${process.env.BASE_URL_API}/warehouses/:cmp_uuid/:page?/:perPage?`, warehouseCtrl.getAllCtrl);
-    app.get(`/${process.env.BASE_URL_API}/warehouse/:cmp_uuid/:war_uuid`, warehouseCtrl.getCtrl);
-    app.post(`/${process.env.BASE_URL_API}/warehouse`, warehouseCtrl.insertCtrl);
-    app.put(`/${process.env.BASE_URL_API}/warehouse/:cmp_uuid/:war_uuid`, warehouseCtrl.updateCtrl);
-    app.delete(`/${process.env.BASE_URL_API}/warehouse/:cmp_uuid/:war_uuid`, warehouseCtrl.deleteCtrl);
+    app.get(`/${process.env.BASE_URL_API}/warehouses/:cmp_uuid/:page?/:perPage?`, authMiddleware, warehouseCtrl.getAllCtrl);
+    app.get(`/${process.env.BASE_URL_API}/warehouse/:cmp_uuid/:war_uuid`, authMiddleware, warehouseCtrl.getCtrl);
+    app.post(`/${process.env.BASE_URL_API}/warehouse`, authMiddleware, warehouseCtrl.insertCtrl);
+    app.put(`/${process.env.BASE_URL_API}/warehouse/:cmp_uuid/:war_uuid`, authMiddleware, warehouseCtrl.updateCtrl);
+    app.delete(`/${process.env.BASE_URL_API}/warehouse/:cmp_uuid/:war_uuid`, authMiddleware, warehouseCtrl.deleteCtrl);
 }
 
 export default configureWarehouseRoutes;
