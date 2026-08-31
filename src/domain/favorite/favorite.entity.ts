cba5127def55e01c5f335bf7ad4644fc4a2735e6
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-08-31 15:53:54 -0300

    i add favorite.entity.ts into favorite folder into folder domain to project

---
+++ src/domain/favorite/favorite.entity.ts
@@ -0,0 +1,9 @@
+export interface FavoriteEntity {
+    cmp_uuid: string,
+    pro_uuid: string,
+    prov_uuid: string,
+    usr_uuid: string,
+    fav_uuid: string,
+    fav_createdat: Date,
+    fav_updatedat: Date
+}
